d32d081fc6296eb23a1b7f34d942c0a5c7c32a85
Luis Elizondo <lelizondo@gmail.com>
2013-03-22 13:54:01 -0600

    Initial commit

---
+++ js/script.js
@@ -0,0 +1,29 @@
+(function ($) {
+  Drupal.behaviors.para = {
+    attach: function (context, settings) {
+			$('ul.parallax', context).onePageNav({
+				currentClass: 'active',
+		    changeHash: false,
+		    scrollSpeed: 750,
+		    scrollOffset: 30,
+		    scrollThreshold: 0.5,
+		    filter: function(index) {
+		    	// Filter only the links with a hash
+		    	if(this.hash) {
+		    		return this.hash;
+		    	}
+		    },
+		    easing: 'swing',
+		    begin: function() {
+		        //I get fired when the animation is starting
+		    },
+		    end: function() {
+		        //I get fired when the animation is ending
+		    },
+		    scrollChange: function($currentListItem) {
+		        //I get fired when you enter a section and I pass the list item of the section
+		    }
+			});
+    }
+  };
+})(jQuery);
